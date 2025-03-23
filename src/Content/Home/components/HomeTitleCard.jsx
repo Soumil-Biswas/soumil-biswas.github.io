286913b36a0d6bfb7ee8a1bf03d493ed25b746c5
Soumil Biswas <soumil.biswas19@gmail.com>
2025-03-23 17:36:53 +0530

    Added function to resolve image loading for home page title cards

--- src/Content/Home/components/HomeTitleCard.jsx
+++ src/Content/Home/components/HomeTitleCard.jsx
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 export default function HomeTitleCard({card}) {
 
   const [isHovered, setIsHovered] = useState(false);  
+
+  // Preload hover image when the component mounts
+  useEffect(() => {
+    const img = new Image();
+    img.src = card.hoverImg;
+  }, [card.hoverImg]);
 
   return (
     <Link
